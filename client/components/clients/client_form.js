97f6711552a6270e699f166f7dda8273a52872c2
ALEXIS <alexis>
2023-05-04 21:48:32 -0300

    proyect

---
+++ client/components/clients/client_form.js
@@ -0,0 +1,24 @@
+import { Clients } from '../../../lib/collections/clients';
+import { Productos } from '../../../lib/collections/productos';
+import { Router } from 'meteor/iron:router';
+import SimpleSchema from 'simpl-schema';
+// Required AutoForm setup
+SimpleSchema.extendOptions(['autoform']);
+
+
+
+Template.clientForm.helpers({
+	formCollection() {
+		return Clients;
+	}
+})
+
+Template.clientForm.onCreated(function()
+{
+	AutoForm.addHooks(['clientForm'],{
+		onSuccess: function(operation, result, template)
+		{
+			Router.go('/clients');
+		}
+	});
+})
